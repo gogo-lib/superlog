e1109bbfb16e967c6d13ac0f95f8681ebe103289
ngoctd <trangoc.contact@gmail.com>
2023-02-06 14:15:46 +0700

    update

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 FILE() {
     file_name=("cronjob.go" "init.go" "kafka_writer.go" "logger.go" "structure_logger.go" "unstructure_logger.go")
 
-    mkdir -p httpclient
+    mkdir -p superlog
 
     for name in ${file_name[@]}; do
         file_path="https://raw.githubusercontent.com/gogo-lib/superlog/main/$name"
